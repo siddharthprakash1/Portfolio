0b560c348c6f67aaac59521bd7aaa32f6fbe63a8
Siddharth Prakash <iamsid0011@gmail.com>
2024-06-14 10:47:29 +0530

    commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -20,7 +20,7 @@ const Hero = () => {
                     variants={container(0.5)}
                     initial="hidden"
                     animate="visible"                    
-                    className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">AI-Web Developer</motion.span>
+                    className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-4xl tracking-tight text-transparent">Software Developer</motion.span>
                     <motion.p 
                     variants={container(1)}
                     initial="hidden"
