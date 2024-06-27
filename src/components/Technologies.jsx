7b2aecbaa84d93751c0824d14f184ca0f1c0d060
Siddharth Prakash <iamsid0011@gmail.com>
2024-06-27 14:07:25 +0530

    New technologies animation added

--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -1,4 +1,4 @@
-import { RiReactjsLine } from "react-icons/ri";
+/*import { RiReactjsLine } from "react-icons/ri";
 import { SiPython } from "react-icons/si";
 import { SiCplusplus } from "react-icons/si";
 import { SiTensorflow } from "react-icons/si";
@@ -59,3 +59,64 @@ const Technologies = () => {
 }
 
 export default Technologies
+*/
+import IconCloud from "@/components/magicui/icon-cloud";
+
+const slugs = [
+  "typescript",
+  "javascript",
+  "C",
+  "java",
+  "react",
+  "tensorflow",
+  "C++",
+  "html5",
+  "css3",
+  "Python",
+  "Pytorch",
+  "Pandas",
+  "sql",
+  "adobe",
+  "adobelightroom",
+  "amazonaws",
+  "cplusplus",
+  "csharp",
+  "nginx",
+  "vercel",
+  "c",
+  "meta",
+  "bootstrap",
+  "github",
+  "pytorch",
+  "docker",
+  "git",
+  "jira",
+  "github",
+  "gitlab",
+  "visualstudiocode",
+  "androidstudio",
+  "python",
+  "figma",
+  "7zip",
+];
+
+export function IconCloudDemo() {
+  return (
+    <div className="flex h-full w-full items-center justify-center overflow-hidden bg-background px-20 pb-20 pt-8">
+      <IconCloud iconSlugs={slugs} />
+    </div>
+  );
+}
+
+const Technologies = () => {
+  return (
+    <div className="pb-24">
+      <h2 className="my-20 text-center text-4xl">Technologies</h2>
+      <div className="w-full">
+        <IconCloudDemo />
+      </div>
+    </div>
+  );
+};
+
+export default Technologies;
