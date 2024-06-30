60f6d061aa17adc77fcd83d05250764930e809b6
Siddharth Prakash <iamsid0011@gmail.com>
2024-06-30 13:11:45 +0530

    added new projects

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,15 +1,18 @@
 import project1 from "../assets/projects/cifar.jpg";
-import project2 from "../assets/projects/bert.jpg";
+import project2 from "../assets/projects/compressed.gif";
 import project3 from "../assets/projects/gym.png";
-import project4 from "../assets/projects/openai.jpg";
+import project4 from "../assets/projects/depth_capture.gif";
 import project5 from "../assets/projects/gifmaker_me.gif";
 import project6 from "../assets/projects/dcgan.gif";
 import project7 from "../assets/projects/weather.png";
+import project8 from "../assets/projects/gif2.gif";
+
 export const HERO_CONTENT = `A driven and versatile computer science engineering student with a diverse skill set spanning machine learning, data analytics, full-stack web development, and cloud technologies. Adept in Python, React, Node.js, and AWS, with hands-on experience in developing innovative projects such as facial recognition systems and sports analytics using Monte Carlo simulations. Committed to delivering impactful solutions through a combination of technical expertise, problem-solving abilities, and a passion for continuous learning. Seeking challenging opportunities to apply my skills and contribute to cutting-edge technological advancements.`;
 
 export const ABOUT_TEXT = `Motivated computer science engineering student adept in machine learning, data analytics, and full-stack web development. Proficient in Python, OpenCV, React, Node.js, and AWS, with a proven track record of developing impactful projects like facial recognition systems and sports analytics using Monte Carlo simulations. A natural problem-solver with strong analytical and technical skills. Beyond academics, I pursue photography professionally, capturing the world through my lens, and enjoy an active lifestyle playing badminton and hitting the gym regularly.`;
 
 export const EXPERIENCES = [
+
   {
     year: "Jun-July 2023",
     role: "Ai Intern",
@@ -35,11 +38,19 @@ export const EXPERIENCES = [
 
 export const PROJECTS = [
   {
-    title: "MLTrader: A Machine Learning Trading Bot",
-    image: project5,
+    title: "AiDepthVision",
+    image: project4,
     description:
-      "MLTrader: An automated trading bot that executes trades with SPY ETF based on sentiment analysis of financial news using FinBERT and Alpaca APIs.",
-      technologies: ["FinBERT", "Alpaca Trade API", "Yahoo Finance API", "TensorFlow", "Pandas"],
+      "This project uses the MiDaS depth estimation model to create real-time depth maps from a webcam feed. ",
+    technologies: ["PyTorch", "OpenCV", "Matplotlib","Numpy","Imageio"],
+  },
+
+  {
+    title: "PathFinderVisualization",
+    image: project2,
+    description:
+      "This project is a React-based visualization tool for pathfinding algorithms. It supports Dijkstra's Algorithm and A* Algorithm, allowing users to visualize the steps taken by these algorithms to find the shortest path between two points on a grid.",
+    technologies: ["JupyterNotebook", "Hugging Face", "Bert"],
   },
   {
     title: "Image Generation of Numbers Using MNIST Dataset and DCGAN",
@@ -49,6 +60,13 @@ export const PROJECTS = [
       technologies: ["Python", "TensorFlow", "NumPy", "Matplotlib"]
   },
   {
+    title: "MLTrader: A Machine Learning Trading Bot",
+    image: project5,
+    description:
+      "MLTrader: An automated trading bot that executes trades with SPY ETF based on sentiment analysis of financial news using FinBERT and Alpaca APIs.",
+      technologies: ["FinBERT", "Alpaca Trade API", "Yahoo Finance API", "TensorFlow", "Pandas"],
+  },
+  {
     title: "Gym Equipment Identifier & Exercise Recommender",
     image: project3,
     description:
@@ -56,19 +74,14 @@ export const PROJECTS = [
     technologies: ["TensorFlow", "Manim", "Numpy", "Pandas"],
   },
   {
-    title: " Text-To-Image Generator",
-    image: project4,
+    title: "ShadowSpeak",
+    image: project8,
     description:
-      "Web app generating images from text using OpenAI API. ",
-    technologies: ["HTML", "OpenAI API", "JavaScript"],
+      " A system that utilizes a CNN model to identify gym equipment with 92% accuracy and recommends personalized exercise routines based on the identified equipment.",
+    technologies: ["TensorFlow", "Manim", "Numpy", "Pandas"],
   },
-  {
-    title: " Question And Answer Model",
-    image: project2,
-    description:
-      "This uses the Hugging Face library ’deepset/bert-base-cased-squad2’ for a question answer model. ",
-    technologies: ["JupyterNotebook", "Hugging Face", "Bert"],
-  },
+
+
   {
     title: "Image Classification",
     image: project1,
